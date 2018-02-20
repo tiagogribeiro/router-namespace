c1d7eef4831614ba6505ab8b49ffccfc48f60b1f
Tiago G. Ribeiro <tiago.sistema@yahoo.com>
2018-02-20 11:56:00 -0300

    Adicionado monolog ao projeto
    Adicionado a biblioteca do monolog ao projeto.

--- bootstrap.php
+++ bootstrap.php
@@ -4,13 +4,29 @@ require '../vendor/autoload.php';
 $config = [
         'log.enable' => true,
         'settings' => [
-            'displayErrorDetails' => true
+            'displayErrorDetails' => true,
+            'db_path' => 'storage/',
+            'log_path' => '../logs/log_application.log'
         ]
 ];
 
 $app = new \Slim\App($config);
+
 $container  = $app->getContainer();
 $container['db'] = function($container)
 {
-    $db = new \PHP\PDO("sqlite:data/namespaceDB.db");
+    $path = $container['settings']['db_path'];
+    if (!is_writable($path)){
+        throw new \Exception('O caminho '.$path.' deve ter permissão para escrita e leitura.');
+    }
+    return new \PDO('sqlite:' . $path . 'router.sqlite3', '','',[ \PDO::ATTR_PERSISTENT => true ]);
+};
+
+$container['logger'] = function($container)
+{
+    $path = $container['settings']['log_path'];
+    $logger = new \Monolog\Logger('log_application');
+    $file_handler = new \Monolog\Handler\StreamHandler( $path );
+    $logger->pushHandler($file_handler);
+    return $logger;
 };
