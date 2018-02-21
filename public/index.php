0ccea17495e96a46b18ea92a28ca53af87c419fd
Tiago G. Ribeiro <tiago.sistema@yahoo.com>
2018-02-21 11:40:32 -0300

    Modificado rota de remoção de workspace
    Modificado rota de remoção de workspace onde será removido
    pelo nome e não pelo Id de identificação do BD.

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 use Router\FinderController;
 use Router\WorkSpaceController;
-use Router\ServerController;
 use Router\TokenController;
 
 require '../bootstrap.php';
@@ -15,14 +14,9 @@ $app->get('/token', TokenController::class . ':generator');
 // Finder
 $app->get('/finder/{name}', FinderController::class . ':find');
 
-// Namespace
+// Workspaces
 $app->get('/workspace', WorkSpaceController::class . ':listAll');
 $app->put('/workspace', WorkSpaceController::class . ':add');
-$app->delete('/workspace/{id}', WorkSpaceController::class . ':delete');
-
-// Server
-$app->get('/server', ServerController::class . ':listAll');
-$app->put('/server', ServerController::class . ':add');
-$app->delete('/server/{id}', ServerController::class . ':delete');
+$app->delete('/workspace/{name}', WorkSpaceController::class . ':delete');
 
 $app->run();
